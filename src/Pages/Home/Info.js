cea943506ec874902a48bd8db0c05bfee42b5bfc
capmahin <mahinulhuque1996@gmail.com>
2022-06-12 04:05:14 +0600

    add info card

---
+++ src/Pages/Home/Info.js
@@ -0,0 +1,12 @@
+import React from "react";
+import InfoDetails from "./InfoDetails";
+
+const Info = () => {
+  return (
+    <div>
+      <InfoDetails></InfoDetails>
+    </div>
+  );
+};
+
+export default Info;
